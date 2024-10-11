9c8e3967a062f69a64a9139828910591b93e4d87
YingqiDuan <141370165+YingqiDuan@users.noreply.github.com>
2024-10-10 20:30:00 -0700

    Delete INFO5100_002309930_YingqiDuan directory

--- INFO5100_002309930_YingqiDuan/Exercises/exercise1/src/PartTimeStudent.java
+++
@@ -1,8 +0,0 @@
-// PartTimeStudent.java
-import java.util.ArrayList;
-
-public class PartTimeStudent extends Student {
-    public PartTimeStudent(String name, ArrayList<Double> quizScores) {
-        super(name, quizScores);
-    }
-}
